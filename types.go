6b2c0c192feb84a3f386620aeea690072d456ef3
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-07-26 16:42:20 +0900

    Add UiaChangeInfo

--- types.go
+++ types.go
@@ -1,6 +1,16 @@
 package oleacc
 
+import (
+	"github.com/go-ole/go-ole"
+)
+
 type Point struct {
 	X int32
 	Y int32
+}
+
+type UiaChangeInfo struct {
+	uiaId     int32
+	payload   ole.VARIANT
+	extraInfo ole.VARIANT
 }
