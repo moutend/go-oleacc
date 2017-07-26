945c7e0f2cebcaca3af2aa37185a92d3bd7a9031
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-07-26 13:54:13 +0900

    Fix missing method

--- iuiautomationtextrange2.go
+++ iuiautomationtextrange2.go
@@ -2,16 +2,23 @@ package oleacc
 
 import (
 	"unsafe"
+
+	"github.com/go-ole/go-ole"
 )
 
 type IUIAutomationTextRange2 struct {
-	IUIAutomationTextRange
+	ole.IUIAutomationTextRange
 }
 
 type IUIAutomationTextRange2Vtbl struct {
-	IUIAutomationTextRangeVtbl
+	ole.IUIAutomationTextRangeVtbl
+	ShowContextMenu uintptr
 }
 
 func (v *IUIAutomationTextRange2) VTable() *IUIAutomationTextRange2Vtbl {
 	return (*IUIAutomationTextRange2Vtbl)(unsafe.Pointer(v.RawVTable))
+}
+
+func (v *IUIAutomationTextRange2) ShowContextMenu() error {
+	return ole.NewError(ole.E_NOTIMPL)
 }
